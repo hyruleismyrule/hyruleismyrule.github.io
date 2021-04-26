1631b78d89ce3aad46ab4598e2fad11e7c54f38d
hyruleismyrule <hyruleismyrule@gmail.com>
2021-04-26 16:57:30 -0400

    added normalize css

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 let oLastModif = new Date(document.lastModified);
 
-var options = { year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'};
+var options = { year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
 
 
 // console.log(today.toLocaleDateString("en-US")); // 9/17/2016
