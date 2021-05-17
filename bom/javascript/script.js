3e3962d17c8a5b92b90a2b9c9b73d6502e51c705
hyruleismyrule <hyruleismyrule@gmail.com>
2021-05-17 17:04:01 -0400

    BOM Assignment

---
+++ bom/javascript/script.js
@@ -0,0 +1,23 @@
+
+const input = document.querySelector('input');
+input.focus();
+const addChapterButton = document.getElementById('addChapterButton');
+const list = document.getElementById('list');
+addChapterButton.addEventListener('click', function () {
+    if (input != null) {
+        const listItem = document.createElement('li');
+        listItem.textContent = document.getElementById('chapter').value;
+        document.getElementById('chapter'). value = "";
+        // input.textContent = "";
+        const listButton = document.createElement('button');
+        listButton.textContent = "❌";
+        // delete here
+        listButton.addEventListener('click', function () {
+            listItem.remove();
+            input.focus();
+        });
+        listItem.append(listButton);
+        list.appendChild(listItem);
+    }
+});
+// document.getElementById('chapter').Value
