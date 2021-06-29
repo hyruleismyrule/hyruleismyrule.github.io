90648786c10c6a3bff0522765f5e1d9f158e1cac
hyruleismyrule <hyruleismyrule@gmail.com>
2021-06-28 21:07:40 -0400

    fixed gallery page

--- a-final-weather-project/javascript/javascript.js
+++ a-final-weather-project/javascript/javascript.js
@@ -38,6 +38,32 @@ function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
 
+// Progressive loading images
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
 // Preston's 5 Day forecast
 let openPage = document.getElementById('page-name');
 if (openPage.textContent == "Preston") {
