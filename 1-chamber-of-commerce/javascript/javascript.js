997215c46f542e82d194944dfd17200d5137a231
hyruleismyrule <hyruleismyrule@gmail.com>
2021-07-15 16:16:14 -0400

    added current weather and weather forecast

--- 1-chamber-of-commerce/javascript/javascript.js
+++ 1-chamber-of-commerce/javascript/javascript.js
@@ -20,3 +20,89 @@ let month = oLastModif.toLocaleDateString("en-US", optionsMonth);
 let year = oLastModif.getFullYear();
 let dateString = day + ", " + date + " " + month + " " + year;
 document.getElementById('lastUpdated').innerHTML = dateString;
+
+// Weather Alert Close Button
+function closeAlert() {
+    document.getElementById("alert").setAttribute('class', "hide");
+    
+}
+
+
+// Home - Current Weather API
+// api key
+// 28e498ae581a0633cfff6d7d29104fdb
+// Davenport, FL City Info
+// {
+//     "id": 4152805,
+//     "name": "Davenport",
+//     "state": "FL",
+//     "country": "US",
+//     "coord": {
+//         "lon": -81.601738,
+//         "lat": 28.1614
+//     }
+// }
+// API Address for current weather
+// api.openweathermap.org/data/2.5/weather?id=4152805&appid=28e498ae581a0633cfff6d7d29104fdb&units=imperial
+// API Address for 5 day forecast weather
+// api.openweathermap.org/data/2.5/forecast?id=4152805&appid=28e498ae581a0633cfff6d7d29104fdb&units=imperial
+
+const currentWeatherAPIurl = "//api.openweathermap.org/data/2.5/weather?id=4152805&appid=28e498ae581a0633cfff6d7d29104fdb&units=imperial";
+fetch(currentWeatherAPIurl)
+    .then((response) => response.json())
+    .then((weatherInfo) => {
+        console.log(weatherInfo);
+        document.getElementById('current-weather').textContent = weatherInfo.weather[0].description;
+        document.getElementById('current-temperature').textContent = Math.round(weatherInfo.main.temp) + "°";
+        document.getElementById('current-humidity').textContent = Math.round(weatherInfo.main.humidity) + "%";
+        document.getElementById('current-feels-like').textContent = Math.round(weatherInfo.main.feels_like) + "°";
+    });
+
+// Home - Current Weather API
+const today = new Date();
+const todayDayNumber = today.getDay() + 1;
+const weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+const forecastAPIurl = "//api.openweathermap.org/data/2.5/forecast?id=4152805&appid=28e498ae581a0633cfff6d7d29104fdb&units=imperial";
+fetch(forecastAPIurl)
+    .then((response) => response.json())
+    .then((forecastInfo) => {
+        console.log(forecastInfo);
+        let forecastList = forecastInfo.list;
+        let forecastDayNumber = todayDayNumber;
+        let forcastCount = 0;
+        for (let i = 0; i < forecastList.length && forcastCount <= 2; i++) {
+            let time = forecastList[i].dt_txt;
+            if (time.includes('21:00:00')) {
+                let theDayName = document.createElement('h3');
+                theDayName.textContent = weekday[forecastDayNumber];
+                let theTemp = document.createElement('p');
+                theTemp.textContent = Math.round(forecastList[i].main.temp) + '\xB0';
+                let iconcode = forecastList[i].weather[0].icon;
+                let iconPath = "images/home/forecast-icons/" + iconcode + ".png";
+                let theIcon = document.createElement('img');
+                theIcon.src = iconPath;
+                let theDay = document.createElement('div');
+                theDay.appendChild(theDayName);
+                theDay.appendChild(theIcon);
+                theDay.appendChild(theTemp);
+                theDay.setAttribute('class', "forecast-box");
+                theDayName.setAttribute('class', "forecast-name");
+                theTemp.setAttribute('class', "forecast-temp");
+                theIcon.setAttribute('class', "forecast-icon");
+                document.getElementById('forecast').appendChild(theDay);
+                forcastCount++;
+            }
+            forecastDayNumber += 1;
+            if (forecastDayNumber === 7) {
+                forecastDayNumber = 0;
+            }
+        }
+    });
