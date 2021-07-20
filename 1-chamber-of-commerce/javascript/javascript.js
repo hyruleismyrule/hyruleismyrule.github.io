4f98a6bc66c50bbe51b9d8288463b2d79c5045a2
hyruleismyrule <hyruleismyrule@gmail.com>
2021-07-20 15:51:56 -0400

    completed davenport city page

--- 1-chamber-of-commerce/javascript/javascript.js
+++ 1-chamber-of-commerce/javascript/javascript.js
@@ -51,54 +51,57 @@ weekday[4] = "Thursday";
 weekday[5] = "Friday";
 weekday[6] = "Saturday";
 
-const OneCallWeatherAPIurl = "//api.openweathermap.org/data/2.5/onecall?lat=28.1614&lon=-81.601738&appid=28e498ae581a0633cfff6d7d29104fdb&units=imperial";
-fetch(OneCallWeatherAPIurl)
-    .then((response) => response.json())
-    .then((weatherInfo) => {
-        console.log(weatherInfo);
-        // Current Weather
-        document.getElementById('current-weather').textContent = weatherInfo.current.weather[0].description;
-        document.getElementById('current-temperature').textContent = Math.round(weatherInfo.current.temp) + "°";
-        document.getElementById('current-humidity').textContent = Math.round(weatherInfo.current.humidity) + "%";
-        document.getElementById('current-feels-like').textContent = Math.round(weatherInfo.current.feels_like) + "°";
-        // Weather Forecast
-        let forecastList = weatherInfo.daily;
-        let forecastDayNumber = todayDayNumber;
-        let forcastCount = 0;
-        for (let i = 0; i < forecastList.length && forcastCount <= 2; i++) {
-            let theDayName = document.createElement('h3');
-            theDayName.textContent = weekday[forecastDayNumber];
-            let theTemp = document.createElement('p');
-            theTemp.textContent = Math.round(forecastList[i].temp.day) + '\xB0';
-            let iconcode = forecastList[i].weather[0].icon;
-            let iconPath = "images/home/new-forecast-icons/" + iconcode + ".png";
-            let theIcon = document.createElement('img');
-            theIcon.src = iconPath;
-            let theDay = document.createElement('div');
-            theDay.appendChild(theDayName);
-            theDay.appendChild(theIcon);
-            theDay.appendChild(theTemp);
-            theDay.setAttribute('class', "forecast-box");
-            theDayName.setAttribute('class', "forecast-name");
-            theTemp.setAttribute('class', "forecast-temp");
-            theIcon.setAttribute('class', "forecast-icon");
-            document.getElementById('forecast').appendChild(theDay);
-            forcastCount++;
-            forecastDayNumber += 1;
-            if (forecastDayNumber === 7) {
-                forecastDayNumber = 0;
+ // Weather
+if (document.getElementById('current-weather')) {
+    const OneCallWeatherAPIurl = "//api.openweathermap.org/data/2.5/onecall?lat=28.1614&lon=-81.601738&appid=28e498ae581a0633cfff6d7d29104fdb&units=imperial";
+    fetch(OneCallWeatherAPIurl)
+        .then((response) => response.json())
+        .then((weatherInfo) => {
+            console.log(weatherInfo);
+            // Current Weather
+            document.getElementById('current-weather').textContent = weatherInfo.current.weather[0].description;
+            document.getElementById('current-temperature').textContent = Math.round(weatherInfo.current.temp) + "°";
+            document.getElementById('current-humidity').textContent = Math.round(weatherInfo.current.humidity) + "%";
+            document.getElementById('current-feels-like').textContent = Math.round(weatherInfo.current.feels_like) + "°";
+            // Weather Forecast
+            let forecastList = weatherInfo.daily;
+            let forecastDayNumber = todayDayNumber;
+            let forcastCount = 0;
+            for (let i = 0; i < forecastList.length && forcastCount <= 2; i++) {
+                let theDayName = document.createElement('h3');
+                theDayName.textContent = weekday[forecastDayNumber];
+                let theTemp = document.createElement('p');
+                theTemp.textContent = Math.round(forecastList[i].temp.day) + '\xB0';
+                let iconcode = forecastList[i].weather[0].icon;
+                let iconPath = "images/home/new-forecast-icons/" + iconcode + ".png";
+                let theIcon = document.createElement('img');
+                theIcon.src = iconPath;
+                let theDay = document.createElement('div');
+                theDay.appendChild(theDayName);
+                theDay.appendChild(theIcon);
+                theDay.appendChild(theTemp);
+                theDay.setAttribute('class', "forecast-box");
+                theDayName.setAttribute('class', "forecast-name");
+                theTemp.setAttribute('class', "forecast-temp");
+                theIcon.setAttribute('class', "forecast-icon");
+                document.getElementById('forecast').appendChild(theDay);
+                forcastCount++;
+                forecastDayNumber += 1;
+                if (forecastDayNumber === 7) {
+                    forecastDayNumber = 0;
+                }
             }
-        }
-        // Alert
-        if (weatherInfo.alerts) {
-            document.getElementById("weather-alert-top").textContent = weatherInfo.alerts[0].description;
-            document.getElementById("weather-alert-bottom").textContent = weatherInfo.alerts[0].description;
-        } else {
-            document.getElementById("alert-top").setAttribute('class', "hide");
-            document.getElementById("alert").setAttribute('class', "hide");
-            document.getElementById("weather-top-row").setAttribute('class', "full-width-current");
-        }
-    });
+            // Alert
+            if (weatherInfo.alerts) {
+                document.getElementById("weather-alert-top").textContent = weatherInfo.alerts[0].description;
+                document.getElementById("weather-alert-bottom").textContent = weatherInfo.alerts[0].description;
+            } else {
+                document.getElementById("alert-top").setAttribute('class', "hide");
+                document.getElementById("alert").setAttribute('class', "hide");
+                document.getElementById("weather-top-row").setAttribute('class', "full-width-current");
+            }
+        });
+}
 
 // Davenport Piechart
 google.charts.load('current', { 'packages': ['corechart'] });
@@ -115,8 +118,28 @@ function drawChart() {
     let options = {
         backgroundColor: "transparent",
         fontName: "EB Garamond",
+        chartArea: {width: "100%"},
         legend: {position: 'bottom'}
     };
     let chart = new google.visualization.PieChart(document.getElementById('piechart'));
     chart.draw(data, options);
+}
+
+// Davenport Slideshow
+let slideIndex = [1,1];
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
 }
