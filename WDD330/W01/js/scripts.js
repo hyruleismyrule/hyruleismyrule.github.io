71e15de4e3840a373ae11383bb2dc135766ba7d2
hyruleismyrule <hyruleismyrule@gmail.com>
2022-01-05 15:04:09 -0500

    completed story assignment

--- WDD330/W01/js/scripts.js
+++ WDD330/W01/js/scripts.js
@@ -17,3 +17,23 @@ function displayStory() {
   document.getElementById("name_display").innerHTML = storyName;
   document.getElementById("story_display").innerHTML = storyHTML;
 }
+// Delete Story
+function deleteStory() {
+  let storyName = document.getElementById("name_input").value;
+  localStorage.removeItem(storyName);
+}
+// Delete all Stories
+function deleteStories() {
+  localStorage.clear();
+}
+// Show Saved Stories
+function showStories() {
+  for (let i = 0; i < localStorage.length; i++) {
+    let savedStories = localStorage.key(i);
+    let listItem = document.createElement("p").innerHTML = savedStories + ", ";
+    let listParent = document.getElementById("saved_list");
+    listParent.append(listItem);
+    document.getElementById("saved_title").textContent = "My Stories";
+    console.log(savedStories);
+  }
+}
