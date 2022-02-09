524987a934eceac596d0a702397ba3df0f41d06a
hyruleismyrule <hyruleismyrule@gmail.com>
2022-02-09 17:20:07 -0500

    Co-authored-by: morton-josh <morton-josh@users.noreply.github.com>

--- WDD330/W06/to-do/js/scripts.js
+++ WDD330/W06/to-do/js/scripts.js
@@ -30,9 +30,56 @@
 // localStorage.setItem("completeTasks", JSON.stringify(completeTasks));
 // localStorage.setItem("tasks", JSON.stringify(tasks));
 // 
-// Task object
+// Task array, Task Name and Completed
+// Use splice for deletion
 let tasks = [
     [ "Assignment 1", true],
-    [ "Assignment 2", true]
+    [ "Assignment 2", true],
+    [ "Assignment 3", false],
+    [ "Assignment 4", false]
 ];
+// Show what's in the array
+console.log(tasks);
+// The Element where I want to put the array
+let taskContainer = document.getElementById("task-container");
+refreshList();
+// Add a task to the array
+function addTask() {
+    let newTask = document.getElementById("add-task").value;
+    // tasks.push([newTask, false]);
+    tasks.push([newTask, true]);
+    refreshList();
+}
 
+function refreshList() {
+    document.getElementById("task-container").remove();
+    let listContainer = document.getElementById("list-container");
+    taskContainer = document.createElement("div");
+    taskContainer.setAttribute("id", "task-container");
+    listContainer.prepend(taskContainer);
+    for (let i = 0; i < tasks.length; i++) {
+        // Check for Complete
+        if (tasks[i][1] == true) {
+            let listItem = document.createElement("div");
+            listItem.innerHTML = "<input type='checkbox'><div>" + tasks[i][0] + "</div><i class='fas fa-times'>";
+            taskContainer.appendChild(listItem);
+        }
+        if (tasks[i][1] == false) {
+            let listItem = document.createElement("div");
+            listItem.innerHTML = "<input type='checkbox' checked='true'><div class='complete'>" + tasks[i][0] + "</div><i class='fas fa-times'>";
+            taskContainer.appendChild(listItem);
+        }
+    }
+    // Show how many tasks are left to do
+    document.getElementById("tasksLeft").innerText = countTasksLeft();
+}
+// Count tasks left
+function countTasksLeft() {
+    let tasksCount = 0;
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i][1]) {
+            tasksCount++;
+        }
+    }
+    return tasksCount;
+}
