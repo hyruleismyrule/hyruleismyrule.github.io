66d7de08793e2104a1a73cb4a51c45a9e597e0cb
hyruleismyrule <hyruleismyrule@gmail.com>
2022-02-10 19:02:37 -0500

    auto focus, clear on enter, close button padding

--- WDD330/W06/to-do/js/scripts.js
+++ WDD330/W06/to-do/js/scripts.js
@@ -90,5 +90,6 @@ input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     document.getElementById("enter-button").click();
+    input.value = "";
   }
 });
