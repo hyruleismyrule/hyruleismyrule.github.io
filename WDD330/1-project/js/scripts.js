2a0eb89b7c08e62004f894183b1286eed4a58b72
hyruleismyrule <hyruleismyrule@gmail.com>
2022-03-15 22:28:27 -0400

    save point

--- WDD330/1-project/js/scripts.js
+++ WDD330/1-project/js/scripts.js
@@ -11,49 +11,72 @@ function openMenu() {
     }
 }
 
-// const pokemonAPIurl = "//pokeapi.co/api/v2/pokemon/charizard";
-const pokemonAPIurl = "//pokeapi.co/api/v2/pokemon/charizard";
-fetch(pokemonAPIurl)
-    .then((response) => response.json())
-    .then((pokemonInfo) => {
-        // console.log(pokemonInfo);
-        let name = pokemonInfo.name;
-        // Capitalize the first letter
-        name = name.charAt().toUpperCase() + name.substring(1)
-        // console.log(name.charAt().toUpperCase() + name.substring(1))
-        let artURL = pokemonInfo.sprites.other["official-artwork"].front_default;
-        // let type = pokemonInfo.types[0].type.name;
-        // let firstGame = pokemonInfo.game_indices[0].version.name;
-        // let typeElement = document.createElement("p");
-        // console.log(name);
-        // console.log(type);
-        // console.log(artURL);
-        // console.log(firstGame);
-        let card = document.createElement("div");
-        card.setAttribute('class', "flash-card");
-
-        let nameElement = document.createElement("h2");
-        let imageElement = document.createElement("img");
-
-        nameElement.textContent = name;
-        imageElement.setAttribute('src', artURL);
-        imageElement.setAttribute('alt', name);
-
-        card.appendChild(nameElement);
-        card.appendChild(imageElement);
-
-        document.getElementById("mysets").appendChild(card);
-    });
-
 // Default Flash Cards
-// const pokemonAPIurl = "//pokeapi.co/api/v2/pokemon/charizard";
-const limitedPokemonAPIurl = "//pokeapi.co/api/v2/pokemon?limit=10";
-fetch(limitedPokemonAPIurl)
+let defaultSet = []
+const defaultPokemonAPIurl = "//pokeapi.co/api/v2/pokemon?limit=10";
+fetch(defaultPokemonAPIurl)
     .then((response) => response.json())
     .then((pokemonInfo) => {
         console.log(pokemonInfo);
-        
+        for (let i = 0, len = pokemonInfo.results.length; i < len; i++) {
+            defaultSet.push(pokemonInfo.results[i].name);
+        }
+        console.log(defaultSet);
+        // Create Flash Cards for all 10 pokemon
+        const pokemonAPIurlBase = "//pokeapi.co/api/v2/pokemon/";
+        for (let i = 0, len = defaultSet.length; i < len; i++) {
+            let tempURL = pokemonAPIurlBase + defaultSet[i];
+            fetch(tempURL)
+                .then((response) => response.json())
+                .then((pokemonInfo) => {
+                    // console.log(pokemonInfo);
+                    let name = pokemonInfo.name;
+                    // Capitalize the first letter
+                    name = name.charAt().toUpperCase() + name.substring(1)
+                    let artURL = pokemonInfo.sprites.other["official-artwork"].front_default;
+                    let card = document.createElement("div");
+                    card.setAttribute('class', "flash-card");
+
+                    let nameElement = document.createElement("h2");
+                    let imageElement = document.createElement("img");
+
+                    nameElement.textContent = name;
+                    imageElement.setAttribute('src', artURL);
+                    imageElement.setAttribute('alt', name);
+
+                    card.appendChild(nameElement);
+                    card.appendChild(imageElement);
+
+                    document.getElementById("mysets").appendChild(card);
+                });
+        }
     });
 
-// What I want to do is call an api for each of the pokemon that was already callded here and get thier name and picture, 
-// because I can get thier name but not thier picture.
+
+
+
+// // const pokemonAPIurl = "//pokeapi.co/api/v2/pokemon/charizard";
+// const pokemonAPIurl = "//pokeapi.co/api/v2/pokemon/charizard";
+// fetch(pokemonAPIurl)
+//     .then((response) => response.json())
+//     .then((pokemonInfo) => {
+//         // console.log(pokemonInfo);
+//         let name = pokemonInfo.name;
+//         // Capitalize the first letter
+//         name = name.charAt().toUpperCase() + name.substring(1)
+//         let artURL = pokemonInfo.sprites.other["official-artwork"].front_default;
+//         let card = document.createElement("div");
+//         card.setAttribute('class', "flash-card");
+
+//         let nameElement = document.createElement("h2");
+//         let imageElement = document.createElement("img");
+
+//         nameElement.textContent = name;
+//         imageElement.setAttribute('src', artURL);
+//         imageElement.setAttribute('alt', name);
+
+//         card.appendChild(nameElement);
+//         card.appendChild(imageElement);
+
+//         document.getElementById("mysets").appendChild(card);
+//     });
