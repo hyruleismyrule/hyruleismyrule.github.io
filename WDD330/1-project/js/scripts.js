1dbece5bf2fde764e165162c184c00e764701102
hyruleismyrule <hyruleismyrule@gmail.com>
2022-03-15 20:28:07 -0400

    cse

--- WDD330/1-project/js/scripts.js
+++ WDD330/1-project/js/scripts.js
@@ -11,3 +11,19 @@ function openMenu() {
     }
 }
 // Note: sometime in the future, I want the menu to slowly transition out instead of snapping
+
+// Pokemon Api
+// fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+
+const pokemonAPIurl = "//pokeapi.co/api/v2/pokemon/charmander";
+fetch(pokemonAPIurl)
+    .then((response) => response.json())
+    .then((pokemonInfo) => {
+        console.log(pokemonInfo);
+        console.log(pokemonInfo.name);
+        let pokemonName = pokemonInfo.name;
+        // return pokemonInfo;
+    });
+console.log(pokemonName);
