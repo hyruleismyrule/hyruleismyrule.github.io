64438355a2d5e72a0015d74bc823cc792d8284bd
hyruleismyrule <hyruleismyrule@gmail.com>
2022-03-22 14:06:51 -0400

    2 3 4

---
+++ WDD230/wdd230-portal/lesson2/js/javascript.js
@@ -0,0 +1,10 @@
+let oLastModif = new Date(document.lastModified);
+
+var options = { year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+
+// console.log(today.toLocaleDateString("en-US")); // 9/17/2016
+// console.log(today.toLocaleDateString("en-US", options)); // Saturday, September 17, 2016
+
+document.getElementById('lastUpdated').innerHTML = oLastModif.toLocaleString("en-US", options);
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
