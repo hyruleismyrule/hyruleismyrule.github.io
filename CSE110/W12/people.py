a103624ac6ac67041797d072d99b531c701c15d1
hyruleismyrule <hyruleismyrule@gmail.com>
2022-03-22 13:38:14 -0400

    removed inactive classes to attempt upload

---
+++ CSE110/W12/people.py
@@ -0,0 +1,29 @@
+print()
+
+people = [
+    "Stephanie 36",
+    "John 29",
+    "Emily 24",
+    "Gretchen 54",
+    "Noah 12",
+    "Penelope 32",
+    "Michael 2",
+    "Jacob 10"
+]
+
+youngest_age = 1000
+youngest_person = ""
+
+for person in people:
+    parts = person.split(" ")
+
+    name = parts[0]
+    age = int(parts[1])
+
+    if age < youngest_age:
+        youngest_age = age
+        youngest_person = name
+    
+print(f"The youngest person is {youngest_person}, who is {youngest_age}")
+
+print()
