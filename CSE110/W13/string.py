0d7204207ebe60cb4b338b5bb5b8f2764008d247
hyruleismyrule <hyruleismyrule@gmail.com>
2022-03-29 17:54:54 -0400

    cse

---
+++ CSE110/W13/string.py
@@ -0,0 +1,16 @@
+print()
+message = input("What is your message? ")
+
+def print_regular(message):
+    print(message)
+
+def print_upper(message):
+    print(message.upper())
+
+def print_lower(message):
+    print(message.lower())
+
+print_regular(message)
+print_upper(message)
+print_lower(message)
+print()
--- WDD330/1-project/js/scripts.js
+++ WDD330/1-project/js/scripts.js
@@ -35,9 +35,6 @@ function checkDefaults(localStorage) {
     if (!setTitles) {
         saveDefaultSets();
     }
-    // if (setTitles && !setTitles.includes("Bulbasaur") && !setTitles.includes("Charmander") && !setTitles.includes("Squirtle")) {
-    //     saveDefaultSets();
-    // }
 }
 
 
@@ -95,7 +92,7 @@ function newSetButton() {
 
     let newSetIcon = document.createElement("div");
     newSetIcon.setAttribute("class", "thumbnail-container new");
-    newSetIcon.setAttribute("onclick", "chooseNewSet()");
+    newSetIcon.setAttribute("onclick", "createNewSet()");
 
     let svgContainer = document.createElement("div");
     svgContainer.setAttribute("class", "new-set-icon-container");
@@ -285,4 +282,34 @@ function displayThumbnails() {
     displayTypes = [];
 
     getThumbnail(localStorage);
+}
+
+// User can create a new set
+function createNewSet() {
+    // Removes and Creates new title
+    // let changeTitle = document.getElementById("essentialTitle");
+    // changeTitle.removeChild(changeTitle.firstElementChild);
+    // changeTitle.setAttribute("class", "back-container");
+
+    // let backArrowContainer = document.createElement("div");
+    // backArrowContainer.setAttribute("class", "arrow");
+    // backArrowContainer.setAttribute("onclick", "displayThumbnails()");
+    // let backSvgHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM384 288H205.3l49.38 49.38c12.5 12.5 12.5 32.75 0 45.25s-32.75 12.5-45.25 0L105.4 278.6C97.4 270.7 96 260.9 96 256c0-4.883 1.391-14.66 9.398-22.65l103.1-103.1c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L205.3 224H384c17.69 0 32 14.33 32 32S401.7 288 384 288z" /></svg>'
+    // backArrowContainer.innerHTML = backSvgHTML;
+    // changeTitle.appendChild( backArrowContainer);
+
+    // let newTitle = document.createElement("h1")
+    // newTitle.textContent = "Back";
+    // newTitle.setAttribute("id", "new-title");
+    // changeTitle.appendChild(newTitle);
+
+    // // Removes and creates new essential div
+    // let changeDiv = document.getElementById("essentialDivContainer");
+    // changeDiv.removeChild(changeDiv.firstElementChild);
+    // let newDiv = document.createElement("div")
+    // newDiv.setAttribute("id", "newSet");
+    // changeDiv.appendChild(newDiv);
+
+    // 
+
 }
