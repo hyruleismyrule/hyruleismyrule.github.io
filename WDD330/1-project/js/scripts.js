46b7c9aefdb2073c15d3e40dfdb801677c524f1a
hyruleismyrule <hyruleismyrule@gmail.com>
2022-03-30 23:47:44 -0400

    working on it...

--- WDD330/1-project/js/scripts.js
+++ WDD330/1-project/js/scripts.js
@@ -116,7 +116,7 @@ function viewSet() {
     backArrowContainer.setAttribute("onclick", "displayThumbnails()");
     let backSvgHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM384 288H205.3l49.38 49.38c12.5 12.5 12.5 32.75 0 45.25s-32.75 12.5-45.25 0L105.4 278.6C97.4 270.7 96 260.9 96 256c0-4.883 1.391-14.66 9.398-22.65l103.1-103.1c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L205.3 224H384c17.69 0 32 14.33 32 32S401.7 288 384 288z" /></svg>'
     backArrowContainer.innerHTML = backSvgHTML;
-    changeTitle.appendChild( backArrowContainer);
+    changeTitle.appendChild(backArrowContainer);
 
     let newTitle = document.createElement("h1")
     newTitle.textContent = "Back";
@@ -311,5 +311,18 @@ function createNewSet() {
     // changeDiv.appendChild(newDiv);
 
     // 
+
+}
+
+// https://pokeapi.co/api/v2/generation/{id or name}/
+
+test() 
+async function test() {
+    const pokemonAPIurl = "//pokeapi.co/api/v2/generation";
+    await fetch(pokemonAPIurl)
+        .then((response) => response.json())
+        .then((info) => {
+            console.log(info);
+        });
 
 }
