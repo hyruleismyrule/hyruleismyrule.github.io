6479b1e03c4373360348c207972efb7ec00c53fe
hyruleismyrule <hyruleismyrule@gmail.com>
2022-04-19 13:55:48 -0400

    tire volume

---
+++ CSE111/W01/tire_volume.py
@@ -0,0 +1,9 @@
+import math
+print()
+width = float(input("Enter the width of the tire in mm (ex 205): "))
+aspect_ratio = float(input("Enter the aspect ratio of the tire (ex 60): "))
+diameter = float(input("Enter the diameter of the wheel in inches (ex 15): "))
+print()
+volume = (math.pi * (width ** 2) * aspect_ratio * (width * aspect_ratio + 2540 * diameter)) / 10000000000
+print(f"The approximate volume is {volume:.2f} liters")
+print()
