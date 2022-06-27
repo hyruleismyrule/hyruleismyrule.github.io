751c36da5fd603f34abf81b7a47d1212c33ef223
hyruleismyrule <hyruleismyrule@gmail.com>
2022-06-27 13:57:10 -0400

    python

--- CSE111/W11/add_area_code.py
+++ CSE111/W11/add_area_code.py
@@ -2,7 +2,7 @@ def main():
     try:
         # Open the file phone_numbers.txt for reading and read all
         # of the phone numbers into a list named phone_numbers.
-        phone_numbers = read_list("phone_numbers.txt")
+        phone_numbers = read_list("CSE111/W11/phone_numbers.txt")
 
         # Print the list of phone numbers which will show that
         # some of the phone number don't have an area code.
@@ -13,7 +13,8 @@ def main():
         # phone number that begins with the area code "208-" To do this,
         # call the map function and pass the add_area_code function and
         # the list of phone numbers as arguments to the map function.
-        pass
+
+        new_numbers = list(map(add_area_code, phone_numbers))
 
         # Print the list with the corrected phone numbers.
         print(new_numbers)
@@ -33,7 +34,11 @@ def add_area_code(phone_number):
         "ddd-dddd" or "ddd-ddd-dddd"
     Return: a string of digits formated as "ddd-ddd-dddd"
     """
-    pass
+    
+    if len(phone_number) < 12:
+        phone_number = "208-" + phone_number
+    
+    return phone_number
 
 
 def read_list(filename):
