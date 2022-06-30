85af4cb559a8d72eca46f21259e581e78f2d4b53
hyruleismyrule <hyruleismyrule@gmail.com>
2022-06-29 20:48:12 -0400

    drop rate page

--- Personal/MLQC/standard-gem/js/scripts.js
+++ Personal/MLQC/standard-gem/js/scripts.js
@@ -552,6 +552,12 @@ function backHome() {
         resultsContainer.style.height = null;
         resultsContainer.style.width = null;
     }
+    if (document.getElementById("dropInfo")) {
+        let infoContainer = document.getElementById("infoContainer");
+        while (infoContainer.firstChild) {
+            infoContainer.removeChild(infoContainer.firstChild);
+        }
+    }
 }
 
 function displayThumbnails(karmas) {
@@ -1139,10 +1145,10 @@ function dropRate() {
     dropInfo.setAttribute("id", "dropInfo");
     dropInfo.innerHTML = 
         '<div class="dropInfo">' + 
-            '<div class="back-button" id="backButton">' + 
+            '<div class="back-button" onclick="backHome()">' + 
                 '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" id="backSVG">' + 
                     '<path' + 
-                        'd="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z" />' + 
+                        ' d="M447.1 256C447.1 273.7 433.7 288 416 288H109.3l105.4 105.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L109.3 224H416C433.7 224 447.1 238.3 447.1 256z" />' + 
                 '</svg>' + 
             '</div>' + 
             '<h2>Drop Rates in Wish Tree</h2>' + 
