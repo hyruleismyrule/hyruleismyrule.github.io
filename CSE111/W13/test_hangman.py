fc5b5f4806b7238d7c0b90b8051884892a036084
hyruleismyrule <hyruleismyrule@gmail.com>
2022-07-15 12:47:25 -0400

    hangman

---
+++ CSE111/W13/test_hangman.py
@@ -0,0 +1,10 @@
+# ********************
+# These are my test functions for my HANGMAN GAME
+# ********************
+from hangman import 
+
+import pytest
+
+
+
+pytest.main(["-v", "--tb=line", "-rN", __file__])
